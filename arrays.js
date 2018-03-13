115ddc8c068211351dbbb6cc5abefb14560e4a5d
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 07:16:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray (array, element) {
-  array.pop(element)
+  array.push(element)
   return array
 }
