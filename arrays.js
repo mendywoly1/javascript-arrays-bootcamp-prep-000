549d696373e7be860707922da3fd4a669d00dab9
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 07:07:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 var chocolateBars = ['snickers', 'hundred snickers', 'kitkat', 'skittles' ]
 
 
+function addElementToBeginningOfArray(array, element) {
+  var arr1 = [element, ...array]
+  return arr1
+}
