9d18d857efe75f5ee5f7e7d59953c215f80dffbf
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-13 07:26:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-    arr4 = array.slice(0, array.legnth)
+    arr4 = array.pop(-1)
   return arr4
 }
 
